780ae47ffe6419f2bdef113e9e3df98a3b8189bc
Arpit <arpitmh23@gmail.com>
2022-02-16 18:34:02 +0530

    Using rest controller to demonstrate http method and respond message to user

---
+++ src/main/java/com/bridgelabz/springboot/springsuit/restapi/controller/HelloRestController.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.springboot.springsuit.restapi.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/web")
+public class HelloRestController {
+
+    @GetMapping("/message")
+    public String message() {
+        return "Hello From BridgeLabz";
+    }
+}
---
+++ src/test/java/com/bridgelabz/springboot/springsuit/restapi/RestapicallsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springboot.springsuit.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapicallsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
