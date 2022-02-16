b3bf040b747a8c40c01671436e0201233084f7cd
Arpit <arpitmh23@gmail.com>
2022-02-16 18:58:13 +0530

    Rest call to show message pass using postmapping for first name and last name

--- src/main/java/com/bridgelabz/springboot/springsuit/restapi/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/springboot/springsuit/restapi/controller/HelloRestController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.springboot.springsuit.restapi.controller;
 
 
+import com.bridgelabz.springboot.springsuit.restapi.model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,6 +21,11 @@ public class HelloRestController {
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + " From BridgeLabz";
+    }
+
+    @PostMapping("/post")
+    public String userData(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
 
 }
